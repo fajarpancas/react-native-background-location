c2b323c238dbc94eefbd334bbadd9fb3bc90b3b6
fajarpancas <fajarpancasaputra@gmail.com>
2026-07-01 15:03:44 +0700

    fix: update Expo plugin to directly add pod to Podfile

--- plugin/withBackgroundLocation.js
+++ plugin/withBackgroundLocation.js
@@ -1,9 +1,42 @@
-const { withPodfileProperties } = require('@expo/config-plugins');
+const {
+  withDangerousMod,
+  withPlugins,
+} = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+function withBackgroundLocationPod(config) {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const podfilePath = path.join(
+        config.modRequest.platformProjectRoot,
+        'Podfile',
+      );
+
+      if (!fs.existsSync(podfilePath)) {
+        return config;
+      }
+
+      let podfile = fs.readFileSync(podfilePath, 'utf-8');
+
+      const podLine =
+        "\n  pod 'VspiritBackgroundLocation', :path => '../node_modules/@fajarpancas/react-native-background-location/ios'\n";
+
+      if (!podfile.includes('VspiritBackgroundLocation')) {
+        podfile = podfile.replace(
+          /(use_expo_modules!.*?\n)/,
+          `$1${podLine}`,
+        );
+        fs.writeFileSync(podfilePath, podfile);
+      }
+
+      return config;
+    },
+  ]);
+}
 
 module.exports = function withBackgroundLocation(config) {
-  config = withPodfileProperties(config, (config) => {
-    config.modResults['EXPO_USE_COMMUNITY_AUTOLINKING'] = '1';
-    return config;
-  });
+  config = withBackgroundLocationPod(config);
   return config;
 };
